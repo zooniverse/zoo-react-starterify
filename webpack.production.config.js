212b2767477c9fc81f742c70a5ef289e465216fa
Roger Hutchings <roger@hutchin.gs>
2016-01-19 12:47:30 +0000

    Add GTM code in production

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -24,6 +24,7 @@ module.exports = {
       template: 'src/index.tpl.html',
       inject: 'body',
       filename: 'index.html',
+      gtm: "<noscript><iframe src=\"//www.googletagmanager.com/ns.html?id=GTM-WDW6V4\" height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe></noscript><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\"gtm.start\":new Date().getTime(),event:\"gtm.js\"});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!=\"dataLayer\"?\"&l=\"+l:\"\";j.async=true;j.src=\"//www.googletagmanager.com/gtm.js?id=\"+i+dl;f.parentNode.insertBefore(j,f);})(window,document,\"script\",\"dataLayer\",\"GTM-WDW6V4\");</script>",
     }),
     new ExtractTextPlugin('[name]-[hash].min.css'),
     new webpack.optimize.UglifyJsPlugin({
